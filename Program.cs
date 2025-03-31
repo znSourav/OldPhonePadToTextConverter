a5eeda0ec023b08106333c909c5aad77c70e82be
MD. Zannatul Naim Sourav <zannatulnaimsourav@gmail.com>
2025-03-31 16:38:48 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using OldPhonePadToTextConverter.Utility;
+
+namespace OldPhonePadToTextConverter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text and press '#' to process:");
+
+            var input = string.Empty;
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+
+                if (key.KeyChar == '#')
+                {
+                    Console.WriteLine();
+                    var message = TextUtility.OldPhonePad(input);
+                    Console.WriteLine(message);
+                    input = string.Empty;
+                }
+                else
+                {
+                    input += key.KeyChar;
+                }
+            }
+        }
+    }
+}
---
+++ Utility/TextUtility.cs
@@ -0,0 +1,11 @@
+﻿namespace OldPhonePadToTextConverter.Utility
+{
+    public static class TextUtility
+    {
+        public static string OldPhonePad(string input)
+        {
+            var message = string.Empty;
+            return message;
+        }
+    }
+}
